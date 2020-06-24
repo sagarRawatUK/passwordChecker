560da14502a0dd0d7b7b814e5ba630192cdf21b7
sagarRawatUK <sagarrawatuk@gmail.com>
2020-06-24 11:32:47 +0530

    Added clear Buton

--- passwordChecker.py
+++ passwordChecker.py
@@ -4,11 +4,16 @@ import sys
 from tkinter import *
 
 def request_api_data(query):
-    url = 'https://api.pwnedpasswords.com/range/' + query
-    res = requests.get(url)
-    if res.status_code != 200:
-        raise RuntimeError(f'Error fetching: {res.status_code}, check the API')
-    return res
+    global l
+    try:
+        url = 'https://api.pwnedpasswords.com/range/' + query
+        res = requests.get(url)
+        if res.status_code != 200:
+            raise RuntimeError(f'Error fetching: {res.status_code}, check the API')
+        return res
+    except:
+        l = Label(root,text="Internet Connection Required!")
+        l.pack()
 
 def get_leaks(hashes, password):
     hashes = (line.split(':') for line in hashes.text.splitlines())
@@ -24,7 +29,12 @@ def pwned_api_check(password):
     response = request_api_data(first5)
     return get_leaks(response , tail)
 
+def clear():
+    password_entry.delete(0,END)
+    l.destroy()
+
 def check_pwd():
+    global l
     password = passwords.get()
     if password != "" :
         count = pwned_api_check(password)
@@ -40,18 +50,27 @@ def check_pwd():
 
 
 root = Tk()
-root.geometry("500x300")
+root.geometry("500x400")
 root.title("Password Checker")
 font = ("poppins",25,"normal")
-Label(root,text="Password Checker",width="50",bg="gray",font=("Calibri",18,"bold")).pack()
+Label(root,text="Password Checker",width="50",height="2",bg="medium sea green",fg="white",font=("Calibri",22,"bold")).pack()
 Label(root,text="").pack()
 Label(root,text="").pack()
-Label(root,text="Enter Password : ",font=("Calibri",18)).pack()
+Label(root,text="Enter Password : ",fg="gray26",font=("Calibri",16,"bold")).pack()
 passwords = StringVar()
-password_entry = Entry(root,textvariable = passwords,width="30")
+password_entry = Entry(root,textvariable = passwords,width="44")
 password_entry.pack()
 Label(root,text="").pack()
-Button(root,text="Search",command=check_pwd,font=("Calibri",12,"bold")).pack()
+
+top = Frame(root)
+bottom = Frame(root)
+top.pack(side=TOP)
+bottom.pack(side=BOTTOM, fill=BOTH, expand=True)
+
+
+b1=Button(root,text="Search",command=check_pwd,width="8",fg="gray26",font=("Calibri",12,"bold")).pack(in_=top, side=LEFT)
+Label(root,text="").pack(in_=top, side=LEFT)
+b2=Button(root,text="Clear",command=clear,width="8",fg="gray28",font=("Calibri",12,"bold")).pack(in_=top, side=LEFT)
 Label(root,text="").pack()
 
 root.mainloop()
